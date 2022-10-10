4a4d1dec48f61859dda774395f6658278ea6faa8
rwietter <mauriciobw17@gmail.com>
2022-10-10 10:56:04 -0300

    fix: remove host

--- apps/server/lib/server.ts
+++ apps/server/lib/server.ts
@@ -4,7 +4,7 @@ const PORT = Number(process.env.PORT) || 8396;
 
 const start = async () => {
   try {
-    await app.listen({ port: PORT, host: `127.0.0.1` });
+    await app.listen({ port: PORT });
   } catch (err: any) {
     console.log(err.message);
     process.exit(1);
