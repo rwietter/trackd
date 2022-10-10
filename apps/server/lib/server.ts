701e45b75c84fc0198a52de97a62a51f3cfe2048
rwietter <mauriciobw17@gmail.com>
2022-10-10 12:41:42 -0300

    fix: app

--- apps/server/lib/server.ts
+++ apps/server/lib/server.ts
@@ -1,10 +1,13 @@
 import { app } from "./app";
 
 const PORT = Number(process.env.PORT) || 3000;
+const HOST = `0.0.0.0`;
 
 const start = async () => {
   try {
-    const response = await app.listen({ port: PORT }) as unknown as Promise<string>;
+    const response = await app.listen(
+      { port: PORT, host: HOST },
+    ) as unknown as Promise<string>;
     console.log(`Yup!!!! Server running at ${response}`);
   } catch (err: any) {
     console.log(err.message);
